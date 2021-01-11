9d2a3d62f73c6610c72af050bec4a272fe5b5393
dimaunix <dimaunix@gmail.com>
2021-01-11 20:59:23 +0100

    renamed

--- eventWidget.py
+++ eventWidget.py
@@ -2,7 +2,8 @@ from PyQt5.QtWidgets import QWidget
 from PyQt5 import uic
 import os
 
+
 class MyEventWidget(QWidget):
     def __init__(self, *args, **kwargs):
         super(MyEventWidget, self).__init__(*args, **kwargs)
-        self.ui = uic.loadUi(os.path.join(os.path.dirname(__file__), "MyEventWidget.ui"), self)
+        self.ui = uic.loadUi(os.path.join(os.path.dirname(__file__), "myEventWidget.ui"), self)
