0bc151d1ba0a84da835370005e75e1e57fc155ee
dimaunix <dimaunix@gmail.com>
2021-01-13 12:43:52 +0100

    build takes version from version.txt

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-create-version-file metadata.yml --outfile file_version_info.txt --version $1
+version=`cat version.txt`
+create-version-file metadata.yml --outfile file_version_info.txt --version $version
 
 pyinstaller --onefile --icon=icon.ico --windowed pyEMG.py --name=pyEMG --add-data "version.txt;." --add-data "icon.ico;." -w --version-file=file_version_info.txt
 
