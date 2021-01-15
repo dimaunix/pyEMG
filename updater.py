bca8ea36a9b05ceb9b50884971fa19853bd5c25c
dimaunix <dimaunix@gmail.com>
2021-01-15 01:48:20 +0100

    added backuping of files and fixed updater being blocked when trying to replace files

--- updater.py
+++ updater.py
@@ -19,6 +19,11 @@ class Updater(QWidget):
         self.widget.ui.setupUi(self.widget)
         self.widget.ui.progressBar.hide()
         self.widget.ui.btnDownload.clicked.connect(self.download_latest_release)
+        self.widget.ui.btnCancel.clicked.connect(self.widget.close)
+        self.latest_version = helper.get_latest_version()
+        self.current_version = helper.get_version()
+        self.widget.ui.lblOldVersion.setText(self.current_version)
+        self.widget.ui.lblNewVersion.setText(self.latest_version)
 
     def Handle_Progress(self, blocknum, blocksize, totalsize):
         readed_data = blocknum * blocksize
@@ -37,8 +42,12 @@ class Updater(QWidget):
         link = "https://github.com/dimaunix/pyEMG/releases/download/" + release_version + "/" + file_name
         if self.download(link, file_name):
             if self.unpack(file_name):
-                if self.replace_files():
-                    self.restart_app()
+                if self.backup_files():
+                    if self.replace_files():
+                        self.restart_app()
+        else:
+            self.widget.ui.btnDownload.setText("Try again")
+            self.widget.ui.btnDownload.setDisabled(False)
 
     def download(self, link, filename):
         self.widget.ui.labelStatus.setText("Downloading...")
@@ -49,7 +58,6 @@ class Updater(QWidget):
             return True
         except Exception as e:
             self.widget.ui.labelStatus.setText("Download error: " + str(e))
-            print(link, filename)
             self.widget.ui.progressBar.hide()
         return False
 
@@ -61,6 +69,24 @@ class Updater(QWidget):
                 return True
         except Exception as e:
             self.widget.ui.labelStatus.setText("Unpacking error: " + str(e))
+        return False
+
+    def backup_files(self):
+        if not os.path.exists("backup"):
+            os.makedirs("backup")
+        self.widget.ui.labelStatus.setText("Creating backup...")
+        try:
+            src = os.path.curdir
+            dst = os.path.join(src, "backup")
+            exclude = {"temp", "backup"}
+            for item in os.listdir(src):
+                if os.path.relpath(item, '.') not in exclude:
+                    s = os.path.join(src, item)
+                    d = os.path.join(dst, item)
+                    shutil.move(s, d)
+            return True
+        except Exception as e:
+            self.widget.ui.labelStatus.setText("Backup error: " + str(e))
         return False
 
     def replace_files(self):
@@ -84,7 +110,6 @@ class Updater(QWidget):
     def restart_app(self):
         self.widget.ui.labelStatus.setText("Restarting...")
         try:
-            # os.execv(sys.argv[0], sys.argv)
             os.execv(sys.executable, [sys.executable, '"' + sys.argv[0] + '"'] + sys.argv[1:])
         except Exception as e:
             self.widget.ui.labelStatus.setText("Restart error: " + str(e))
